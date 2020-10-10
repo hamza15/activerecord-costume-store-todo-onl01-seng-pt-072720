f15ce9ee1a0aa8ae582a32de5489450ab0178a65
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-10 22:10:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   
   
   def change
-    create_table :costume_stores do |t|
+    create_table :comstume_stores do |t|
       t.string :name
       t.string :location
       t.integer :costume_inventory
